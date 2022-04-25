390556bd3b7595661982ce6607636d0b4cd0d383
silje <angelviksilje@gmail.com>
2022-04-25 15:30:54 +0200

    sir-commit-not-a-lotjjj

--- src/index.js
+++ src/index.js
@@ -1,58 +1,97 @@
-const url = "https://www.avena.dev/cms/wp-json/wp/v2/tribe_events";
-const urlPosts = "https://www.avena.dev/cms/wp-json/wp/v2/posts?per_page=25";
-const urlCategories = "https://www.avena.dev/cms/wp-json/wp/v2/categories?per_page=25";
-const urlTags = "https://www.avena.dev/cms/wp-json/wp/v2/tags?per_page=25";
-
-
+const url = "https://www.avena.dev/cms/wp-json/wp/v2/tribe_events?per_page=50";
 const out = document.querySelector("#eventsList");
-//declaring api's
 
-let months = ["JAN", "FEB", "MAR", "APR", "MAI",
-    "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DES"];
-
-/*
-let eventDate = new Date(event.date);
-let eventMonth = eventDate.getMonth();
-
-<p>eventDate.getDate();</p>
-<p>eventDate.getMonth();</p>
-
-<p> months[eventMonth]; </p>
-*/
-
-//declaring global values
-/*
-let tag = [];
-let post = [];
-let categories = [];
-let processedPosts = "";
-*/
-
-
+let allPosts;
 fetch(url)
     .then((response) => response.json())
     .then((parsedData) => {
         listData(parsedData);
+        allPosts = parsedData;
     })
     .catch((error) => (out.innerHTML = "Something's wrong! " + error))
     .finally(() => document.querySelector(".loader").remove());
 
 
-function listData(events) {
-    events.forEach((event) => {
-        console.log(event.title);
-
-        out.innerHTML += `<div id="posts" class="displayDate">
-            <a href="detail.html?id=${event.id}">
-                <span id="outputMonths">${event.date}</span>
-            </a>
-        </div>
-    <div>
-        <h2><a href="detail.html?id=${event.id}">${event.title.rendered}</a></h2>
-        <p>${event.excerpt.rendered}</p>
-        <p>${event.tribe_events_cat}</p>
-</div>
-</div>`;
+function listData(posts) {
+    out.innerHTML = " ";
+    posts.forEach((post) => {
+      //  console.log(post.title.length, post.title);
+        out.innerHTML +=
+        `<div>
+                <div id="dateBox">
+                    <a href="detail.html?id=${post.id}">
+                        <span>${post.date}</span>
+                    </a>
+            </div>
+            <div>
+                <a href="detail.html?id=${post.id}"><h3>${post.title.rendered}</h3>
+                <p id="postExcerpt">${post.excerpt.rendered}</p></a>
+            </div>
+        </div>`;
     });
 }
 
+
+/* FILTER ALL EVENTS,  ALL POSTS WITH CATEGORY ID'S  */
+const myAllBtn = document.querySelectorAll(".filterAll");
+
+//console.log(myStudBtn)
+for (let item of myAllBtn) {
+    item.addEventListener("click", () => {
+        // console.log("skal filtrere ut alle eventer", allPosts.length )
+        listData(allPosts);
+    })
+}
+
+/* FILTER STUDENT EVENTS,  STUDENT CAT ID = 95 */
+const myStudBtn = document.querySelectorAll(".filterStudent");
+//console.log(myStudBtn)
+for (let item of myStudBtn) {
+    item.addEventListener("click", () => {
+      // console.log("skal filtrere ut studenter", allPosts.length )
+        let filteredList = [];
+        allPosts.forEach((post) => {
+           // console.log(post.tribe_events_cat)
+            if (post.tribe_events_cat.includes(95)) {
+                filteredList.push(post);
+            }
+        })
+        listData(filteredList);
+    })
+}
+
+/* FILTER ANSATT EVENTS,  STUDENT CAT ID = 96  */
+const myAnsattBtn = document.querySelectorAll(".filterAnsatt");
+//console.log(myAnsattBtn)
+for (let item of myAnsattBtn) {
+    item.addEventListener("click", () => {
+        // console.log("skal filtrere ut ansatte", allPosts.length )
+        let filteredList = [];
+        allPosts.forEach((post) => {
+            // console.log(post.tribe_events_cat)
+            if (post.tribe_events_cat.includes(96)) {
+                filteredList.push(post);
+            }
+        })
+        listData(filteredList);
+    })
+}
+
+/* FILTER STYRET EVENTS,  STUDENT CAT ID = 97  */
+const myStyretBtn = document.querySelectorAll(".filterStyret");
+//console.log(myStyretBtn)
+for (let item of myStyretBtn) {
+    item.addEventListener("click", () => {
+        // console.log("skal filtrere ut styret", allPosts.length )
+        let filteredList = [];
+        allPosts.forEach((post) => {
+            // console.log(post.tribe_events_cat)
+            if (post.tribe_events_cat.includes(97)) {
+                filteredList.push(post);
+            }
+        })
+        listData(filteredList);
+    })
+}
+
+// console.log(filteredList)
